c1eec5b1f8bcbf84af4ed0507d6ccb45c167daf0
Ufuoma Okoro <ufookoro@gmail.com>
2019-08-17 00:10:38 +0100

    Initial Commit

---
+++ VideoRentals/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoRentals
+//
+//  Created by Ufuoma Okoro on 17/08/2019.
+//  Copyright © 2019 Ufuoma Okoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
