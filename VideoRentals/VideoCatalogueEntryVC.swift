9e8081ac9054d519145a2fb03e874344ebd2a428
Ufuoma Okoro <ufookoro@gmail.com>
2019-08-18 19:05:52 +0100

    (1) Create Video Entry Form - Tested successfully

--- VideoRentals/VideoCatalogueEntryVC.swift
+++ VideoRentals/VideoCatalogueEntryVC.swift
@@ -7,18 +7,37 @@
 //
 
 import UIKit
+import CoreData
+
 
 class VideoCatalogueEntryVC: UIViewController {
     
     //Outlet for Record Entry
     @IBOutlet weak var backToMainPageOutletItem: UIBarButtonItem!
+    @IBOutlet weak var navigationBarOutletNav: UINavigationBar!
+    @IBOutlet weak var imageOutletView: UIImageView!
+    @IBOutlet weak var cataloguOutletText: UITextField!
+    @IBOutlet weak var filmTitleOutleuText: UITextField!
+    @IBOutlet weak var fileGenreOutletLabel: UILabel!
+    @IBOutlet weak var genrePickOutletPickerView: UIPickerView!
+    @IBOutlet weak var quantityOutletText: UITextField!
+    @IBOutlet weak var rentalCostOutletText: UITextField!
+    @IBOutlet weak var addFilmOutletButton: UIButton!
+    @IBOutlet weak var genreOutletLabel: UILabel!
+    
+    
+    
+    //Variable for Genre - PickerView
+    var filmGenreArray = ["Horror","Thriller","Comedy","Action","General"]
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Custom function
-        formatVC()
+        formatVC() //Format Textfield and Buttons
+        conPickerView() //Connect Pickerview
     }
     
     
@@ -41,8 +60,101 @@ class VideoCatalogueEntryVC: UIViewController {
         
         //Set the background for View Controller
         self.view.backgroundColor = #colorLiteral(red: 0.506442913, green: 0.3871898624, blue: 0.574860394, alpha: 0.94)
+        
+        //Format the navigation bar
+        navigationBarOutletNav.layer.shadowOpacity = 1
+        navigationBarOutletNav.layer.shadowRadius = 5
+        navigationBarOutletNav.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        //Format fields
+        cataloguOutletText.textColor = UIColor.blue
+        cataloguOutletText.autocapitalizationType = .allCharacters
+        cataloguOutletText.layer.shadowOpacity = 1
+        cataloguOutletText.layer.shadowRadius = 5
+        cataloguOutletText.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+       
+        filmTitleOutleuText.layer.shadowOpacity = 1
+        filmTitleOutleuText.layer.shadowRadius = 5
+        filmTitleOutleuText.layer.shadowOffset = CGSize(width: 3, height: 3)
+        filmTitleOutleuText.clearButtonMode = .always
+        
+        fileGenreOutletLabel.layer.shadowOpacity = 1
+        fileGenreOutletLabel.layer.shadowRadius = 5
+        fileGenreOutletLabel.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        imageOutletView.image = UIImage(named: "movieProjectorIcon.png")
+        
+        
+        self.genrePickOutletPickerView.backgroundColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+        genrePickOutletPickerView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        genrePickOutletPickerView.layer.shadowOpacity = 1
+        genrePickOutletPickerView.layer.shadowRadius = 5
+        genrePickOutletPickerView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        genrePickOutletPickerView.layer.cornerRadius = 15
+        genrePickOutletPickerView.layer.masksToBounds = false
+        
+        
+        
+        quantityOutletText.layer.shadowOpacity = 1
+        quantityOutletText.layer.shadowRadius = 5
+        quantityOutletText.layer.shadowOffset = CGSize(width: 3, height: 3)
+        quantityOutletText.clearButtonMode = .always
+        
+        rentalCostOutletText.layer.shadowOpacity = 1
+        rentalCostOutletText.layer.shadowRadius = 5
+        rentalCostOutletText.layer.shadowOffset = CGSize(width: 3, height: 3)
+        rentalCostOutletText.clearButtonMode = .always
+        
+        //Format the Add Button
+        
+        addFilmOutletButton.setTitleColor(.black, for: .normal)
+        addFilmOutletButton.backgroundColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+        addFilmOutletButton.layer.shadowOpacity = 1
+        addFilmOutletButton.layer.shadowRadius = 5
+        addFilmOutletButton.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
+        
+        
+        
+        
+    }
+    
+    //Connect PickerView
+    
+    func conPickerView() {
+        
+        //Connect DataSource and Delegate
+        genrePickOutletPickerView.delegate = self
+        genrePickOutletPickerView.dataSource = self
     }
     
 
 
+}
+
+extension VideoCatalogueEntryVC: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //Set number of sections
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Set number of rows
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return filmGenreArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return filmGenreArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        fileGenreOutletLabel.textColor = UIColor.red
+        fileGenreOutletLabel.text = filmGenreArray[row]
+    }
+    
+    
+    
 }
